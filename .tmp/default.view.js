7bc8c3b767bd0ccd8b24c824214b552531819639
lizhuo13 <lizhuo13@meituan.com>
2022-06-15 20:51:55 +0800

    feat: 更新设置

---
+++ .tmp/default.view.js
@@ -0,0 +1,23 @@
+
+
+
+import * as componentInstances from '/Users/lizhuo13/lowcode/lowcode-single-com/src/index.tsx';
+
+import '/Users/lizhuo13/lowcode/lowcode-single-com/src/index.scss'
+
+export * from '/Users/lizhuo13/lowcode/lowcode-single-com/src/index.tsx';
+
+const coveredComponents = {};
+
+const library = 'BizComp';
+const execCompile = !!false;
+
+if (!execCompile) {
+  window[library] = Object.assign({__esModule: true}, componentInstances || {}, coveredComponents || {});
+}
+
+function getRealComponent(component, componentName) {
+  if (component.default) return component.default;
+  if (component[componentName]) return component[componentName];
+  return component;
+}
